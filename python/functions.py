b3266c2385e6877920d7d6be2f0c09ede88a40bd
Edmund Xiang Lin Ting <edmund.xiang.lin.ting@cern.ch>
2025-07-11 12:13:24 +0930

    Reduce duplicated code

--- python/functions.py
+++ python/functions.py
@@ -1,16 +1,35 @@
 import numpy as np
 
-# Counts the number of consecutive days that the unit price increased or decreased
-# Returns two lists `num_consec_incr, num_consec_decr` containing the counts for each case
-def countConsecutiveMovement(price):
-    num_consec_incr = []
-    num_consec_decr = []
+# Analyse consecutive movement of the unit price
+def consecutiveMovement(price):
+    # Initialise lists to be returned
+    num_consec_incr = [] # number of consecutive price increases
+    num_consec_decr = [] # number of consecutive price decreases
+    slope_incr = []      # linear gradient of the increasing period
+    slope_decr = []      # linear gradient of the decreasing period
+
+    # Loop over the prices and track consecutive periods
     num_incr = num_decr = 0
     price = pruneLeadingBears(price)
     for i in range(1,len(price)):
+        # append quantities of interest to corresponding outputs
         if price[i] > price[i-1] and num_decr > 0:
+            # number of consecutive increases and decreases
             num_consec_incr.append(num_incr)
             num_consec_decr.append(num_decr)
+
+            # fit 1st order polynomials to extract linear gradient
+            increasing_period = list(range(num_incr+1))
+            increasing_price = price[i-1-num_decr-num_incr:i-num_decr]
+            increasing_slope = np.polyfit(increasing_period, increasing_price, 1)[0]
+            slope_incr.append(increasing_slope)
+            
+            decreasing_period = list(range(num_decr+1))
+            decreasing_price = price[i-1-num_decr:i]
+            decreasing_slope = np.polyfit(decreasing_period, decreasing_price, 1)[0]
+            slope_decr.append(decreasing_slope)
+            
+            # reset counters
             num_incr = num_decr = 0
 
         # increment counters
@@ -20,49 +39,8 @@ def countConsecutiveMovement(price):
         if i == len(price)-1:
             if num_incr > 0: num_consec_incr.append(num_incr)
             if num_decr > 0: num_consec_decr.append(num_decr)
-    return num_consec_incr, num_consec_decr
 
-# Number of consecutive decreases that follow each increasing period ("momentum" of the flip)
-def flipMomentum(price):
-    momentum = []
-    num_incr = num_decr = 0
-    price = pruneLeadingBears(price)
-    for i in range(1,len(price)):
-        # if prices start to increase again after a period of decreases,
-        # append the counts to the 2D array and restart the process
-        if price[i] > price[i-1] and num_decr > 0:
-            momentum.append([num_incr, num_decr])
-            num_incr = num_decr = 0
-
-        # increment counters
-        num_incr, num_decr = incrementCounts(num_incr, num_decr, price[i], price[i-1])
-
-    return momentum
-
-# Slopes of successive increasing -> decreasing periods ("strength" of the flip)
-def flipStrength(price):
-    strength = []
-    num_incr = num_decr = 0
-    price = pruneLeadingBears(price)
-    for i in range(1,len(price)):
-        # if prices start to increase again after a period of decreases,
-        # append the ratio of their slopes then restart the process
-        if price[i] > price[i-1] and num_decr > 0:
-            increasing_period = list(range(num_incr+1))
-            increasing_price = price[i-1-num_decr-num_incr:i-num_decr]
-            increasing_slope = np.polyfit(increasing_period, increasing_price, 1)[0]
-
-            decreasing_period = list(range(num_decr+1))
-            decreasing_price = price[i-1-num_decr:i]
-            decreasing_slope = np.polyfit(decreasing_period, decreasing_price, 1)[0]
-
-            strength.append([increasing_slope, decreasing_slope])
-            num_incr = num_decr = 0
-
-        # increment counters
-        num_incr, num_decr = incrementCounts(num_incr, num_decr, price[i], price[i-1])
-        
-    return strength
+    return num_consec_incr, num_consec_decr, slope_incr, slope_decr
     
 # Trim the start of a list so that it begins with a period of increasing price
 def pruneLeadingBears(price):
@@ -77,11 +55,11 @@ def pruneLeadingBears(price):
 # Increment counters for consecutive increasing / decreasing days
 # If two consecutive days close at the same price, treat as increasing or decreasing
 # depending on the trend that is currently being tracked
-def incrementCounts(num_incr, num_decr, today, yesterday):
-    if today > yesterday:
+def incrementCounts(num_incr, num_decr, current, previous):
+    if current > previous:
         return num_incr+1, num_decr
-    elif today < yesterday:
+    elif current < previous:
         return num_incr, num_decr+1
-    elif today == yesterday:
+    elif current == previous:
         if num_decr > 0: return num_incr, num_decr+1
         elif num_incr > 0: return num_incr+1, num_decr
--- python/utils.py
+++ python/utils.py
@@ -2,7 +2,14 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mpl_dates
 from mplfinance.original_flavor import candlestick_ohlc
 
-def plot_candle(ohlc, xlabel="Date", ylabel="Price [$]", width=0.6, colorup='green', colordown='red', alpha=0.8, **kwargs):
+def plot_candle( ohlc,
+                 xlabel = "Date",
+                 ylabel = "Price [$]",
+                 width = 0.6,
+                 colorup = 'green',
+                 colordown = 'red',
+                 alpha = 0.8,
+                 **kwargs ):
     fig, ax = plt.subplots()
     candlestick_ohlc(ax, ohlc.values, width=width, colorup=colorup, colordown=colordown, alpha=alpha, **kwargs)
     date_format = mpl_dates.DateFormatter('%d-%m-%Y')
@@ -10,19 +17,39 @@ def plot_candle(ohlc, xlabel="Date", ylabel="Price [$]", width=0.6, colorup='gre
     fig.autofmt_xdate()
     prettify(xlabel, ylabel, rotate=True)
 
-def plot_line(x, y, xlabel="x", ylabel="y", horizontal=[], **kwargs):
+def plot_line( x,
+               y,
+               xlabel = "x",
+               ylabel = "y",
+               horizontal = [],
+               **kwargs ):
     if horizontal:
         for value in horizontal:
             plt.axhline(y=value, color='lightgrey', linestyle='dashed')
     plt.plot(x, y, **kwargs)
     prettify(xlabel, ylabel, rotate=True)
 
-def plot_hist(values, xlabel="Values", ylabel="Frequency", axisrange=None, nbins=50, density=False, color="royalblue", alpha=0.5, legend="", **kwargs):
+def plot_hist( values,
+               xlabel = "Values",
+               ylabel = "Frequency",
+               axisrange = None,
+               nbins = 50,
+               density = False,
+               color = "royalblue",
+               alpha = 0.5,
+               legend = "",
+               **kwargs ):
     plt.hist(values, range=axisrange, bins=nbins, density=density, color=color, alpha=alpha, label=legend, **kwargs)
     prettify(xlabel, ylabel)
 
-def plot_hist2d(values, xlabel="", ylabel="", zlabel="", text=False, **kwargs):
-    hist, xbins, ybins, image = plt.hist2d(values[:,0], values[:,1], **kwargs)
+def plot_hist2d( x,
+                 y,
+                 xlabel = "",
+                 ylabel = "",
+                 zlabel = "",
+                 text = False,
+                 **kwargs ):
+    hist, xbins, ybins, image = plt.hist2d(x, y, **kwargs)
     cbar = plt.colorbar()
     prettify(xlabel, ylabel)
     if zlabel: cbar.set_label(zlabel)
@@ -32,15 +59,20 @@ def plot_hist2d(values, xlabel="", ylabel="", zlabel="", text=False, **kwargs):
             for j in range(len(xbins)-1):
                 bin_content = int(hist.T[i,j])
                 if bin_content == 0: continue
-                ax.text(xbins[j]+0.5*(xbins[j+1]-xbins[j]),ybins[i]+0.5*(ybins[j+1]-ybins[j]), bin_content, color="w", ha="center", va="center", fontweight="bold")
+                xloc = xbins[j] + 0.5*(xbins[j+1]-xbins[j])
+                yloc = ybins[i] + 0.5*(ybins[j+1]-ybins[j])
+                ax.text(xloc, yloc, bin_content, color="w", ha="center", va="center", fontweight="bold")
 
-def prettify(xlabel="", ylabel="", rotate=False):
+def prettify( xlabel = "",
+              ylabel = "",
+              rotate = False ):
     if xlabel: plt.xlabel(xlabel)
     if ylabel: plt.ylabel(ylabel)
     if rotate: plt.xticks(rotation=45, fontsize='small')
     plt.tight_layout()
 
-def save(name, show=False):
+def save( name,
+          show = False ):
     plt.savefig(f"outputs/{name}.png")
     if show: plt.show()
     else: plt.close()
