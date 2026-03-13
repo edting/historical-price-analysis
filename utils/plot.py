cbffe1a6189fe3935be1c730c579248d829f5828
Edmund Xiang Lin Ting <edmund.xiang.lin.ting@cern.ch>
2026-03-13 20:44:05 +1030

    Cleanup

--- utils/plot.py
+++ utils/plot.py
@@ -4,7 +4,7 @@ import matplotlib.dates as mpl_dates
 import mplfinance as mpf
 from mplfinance.original_flavor import candlestick_ohlc
 
-def set_plot_style(style='fast'):
+def set_style(style='fast'):
     """
     The full list of possible customisations can be found here:
     https://matplotlib.org/stable/users/explain/customizing.html#the-default-matplotlibrc-file
@@ -17,10 +17,10 @@ def set_plot_style(style='fast'):
     mpl.rcParams['axes.labelsize'] = 14
     mpl.rcParams['font.size'] = 11
 
-def plot_candle( ohlc,
-                 xlabel = "Date",
-                 ylabel = "Price [$]",
-                 **kwargs ):
+def candle( ohlc,
+            xlabel = "Date",
+            ylabel = "Price [$]",
+            **kwargs ):
     fig, ax = mpf.plot(ohlc, type='candle', xlabel=xlabel, ylabel=ylabel, figratio=(8,6), returnfig=True, **kwargs)
     ax[0].yaxis.set_label_position('left')
     ax[0].yaxis.tick_left()
@@ -28,14 +28,14 @@ def plot_candle( ohlc,
         ax[0].spines[i].set_color('black')
     prettify(rotate=True)
 
-def plot_candle_original( ohlc,
-                          xlabel = "Date",
-                          ylabel = "Price [$]",
-                          width = 0.6,
-                          colorup = 'green',
-                          colordown = 'red',
-                          alpha = 0.8,
-                          **kwargs ):
+def candle_original( ohlc,
+                     xlabel = "Date",
+                     ylabel = "Price [$]",
+                     width = 0.6,
+                     colorup = 'green',
+                     colordown = 'red',
+                     alpha = 0.8,
+                     **kwargs ):
     fig, ax = plt.subplots()
     candlestick_ohlc(ax, ohlc.values, width=width, colorup=colorup, colordown=colordown, alpha=alpha, **kwargs)
     date_format = mpl_dates.DateFormatter('%d-%m-%Y')
@@ -43,40 +43,40 @@ def plot_candle_original( ohlc,
     fig.autofmt_xdate()
     prettify(xlabel, ylabel, rotate=True)
 
-def plot_line( x,
-               y,
-               xlabel = "x",
-               ylabel = "y",
-               horizontal = [],
-               **kwargs ):
+def line( x,
+          y,
+          xlabel = "x",
+          ylabel = "y",
+          horizontal = [],
+          **kwargs ):
     plt.plot(x, y, **kwargs)
     if horizontal:
         for value in horizontal:
             plt.axhline(y=value, color='lightgrey', linestyle='dashed', zorder=0)
     prettify(xlabel, ylabel, rotate=True)
 
-def plot_hist( values,
-               xlabel = "Values",
-               ylabel = "Frequency",
-               axisrange = None,
-               nbins = 50,
-               density = False,
-               color = "royalblue",
-               alpha = 0.5,
-               legend = "",
-               **kwargs ):
+def hist( values,
+          xlabel = "Values",
+          ylabel = "Frequency",
+          axisrange = None,
+          nbins = 50,
+          density = False,
+          color = "royalblue",
+          alpha = 0.5,
+          legend = "",
+          **kwargs ):
     plt.hist(values, range=axisrange, bins=nbins, density=density, color=color, alpha=alpha, label=legend, **kwargs)
     if legend:
         plt.legend()
     prettify(xlabel, ylabel)
 
-def plot_hist2d( x,
-                 y,
-                 xlabel = "",
-                 ylabel = "",
-                 zlabel = "",
-                 text = False,
-                 **kwargs ):
+def hist2d( x,
+            y,
+            xlabel = "",
+            ylabel = "",
+            zlabel = "",
+            text = False,
+            **kwargs ):
     hist, xbins, ybins, image = plt.hist2d(x, y, **kwargs)
     cbar = plt.colorbar()
     prettify(xlabel, ylabel)
